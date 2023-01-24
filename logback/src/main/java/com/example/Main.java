c64f7642522f19faba493b9e90fc8335d9531ef4
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2023-01-24 09:06:27 +0900

    test

--- logback/src/main/java/com/example/Main.java
+++ logback/src/main/java/com/example/Main.java
@@ -9,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         log.info("Hello");
         log.debug("Debug");
+        log.warn("Warn");
         log.error("Error");
     }
 }
